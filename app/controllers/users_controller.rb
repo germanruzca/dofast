c0b101f6bd7215edea3bd2e9ff11feca7d6d09cc
germanruzca <germanruzca@gmail.com>
2022-04-08 13:18:53 -0500

    Create the controller for users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  
+  def show
+    @user = User.find_by_id(current_user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :state, :country, :bio, :status_id, :role_id, :email)
+  end
+end
